b003b7c0a3364ca174febce83f1673e8ee1eebf2
Robert L. Ryan <1085908428@qq.com>
2025-01-21 18:30:11 +0800

    v1.1.0:can use url array directly

--- src/push.js
+++ src/push.js
@@ -1,15 +1,20 @@
 /* 
  * push.js: make your HTML tidier
+ * Version: 1.1.0
  * Author: Robert L. Ryan(1085908428@qq.com)
- * Time: 2025-01-21T15:32:00Z
+ * Time: 2025-01-21T17:50:00Z
  * Licensed under the GNU GPL License
  */
 ( function( global, DOM ) {
 	"use strict"
 	global.push = function( url ) {
-		var script = DOM.createElement('script'); 
-		var scriptOld = DOM.getElementsByTagName('script')[0]; 
-		script.src = url; 
-		scriptOld.parentNode.insertBefore(script,scriptOld); 
+		if( url instanceof Array ){
+			for(var i=0;i<url.length;i++)push(url[i])
+		} else {
+			var script = DOM.createElement('script'); 
+			var scriptOld = DOM.getElementsByTagName('script')[0]; 
+			script.src = url; 
+			scriptOld.parentNode.insertBefore(script,scriptOld); 
+		};
 	};
 }) ( typeof window === "object" ? window : this, document );
